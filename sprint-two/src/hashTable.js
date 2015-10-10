9b51061181885c26e96c951ed1a7b58f89b2aaa9
Shashi <shashi@Shashis-MacBook-Pro.local>
2015-10-10 01:49:33 -0700

    completed hashTable & sets

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,22 +1,50 @@
 
 
 var HashTable = function() {
-  this._limit = 8;
-  this._storage = LimitedArray(this._limit);
+  this.limit = 8;
+  //this.storage = LimitedArray(this.limit);
+  this.storage = [];
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this.limit);
+  
+  if(this.storage[index] === undefined) {
+    this.storage[index] = ([[k, v]]);  
+  } else {
+    var inserted = false;
+    for(var i = 0; i < this.storage[index].length; i++){
+      if(this.storage[index][i][0] === k){
+        this.storage[index][i][1] = v;
+        inserted = true;
+      }
+    }
+    if(inserted === false) {
+      this.storage[index].push([k,v]);
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this.limit);
+  
+  for(var i = 0; i < this.storage[index].length; i++){
+    if(this.storage[index][i][0] === k){
+      return this.storage[index][i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this.limit);
+  for(var i = 0; i < this.storage[index].length; i++){
+    if(this.storage[index][i][0] === k){
+      this.storage[index][i][1] = null;
+    }
+  } 
 };
 
+var hashTable = new HashTable();
 
 
 /*
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,32 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set.storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  set._storage = undefined;
+  if (this.storage.indexOf(item) < 0) {
+  this.storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  for (var i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  for(var i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] === item){
+      this.storage.splice(i, 1);
+    }
+  }
 };
 
 /*
